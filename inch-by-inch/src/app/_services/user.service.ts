e9ac9eb292b7bb0ef8169101944885e9ab1c009a
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2022-01-30 06:21:20 -0500

    adding user service

--- inch-by-inch/src/app/_services/user.service.ts
+++ inch-by-inch/src/app/_services/user.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+//Provides access to public and private resources
+const API_URL = 'http://localhost:9192/api/test/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPublicContent(): Observable<any> {
+    return this.http.get(API_URL + 'all', {
+      responseType: 'text'
+    });
+  }
+
+  getUserBoard(): Observable<any> {
+    return this.http.get(API_URL + 'user', {
+      responseType: 'text'
+    });
+  }
+
+  getModeratorBoard(): Observable<any> {
+    return this.http.get(API_URL + 'mod', {
+      responseType: 'text'
+    });
+  }
+
+  getAdminBoard(): Observable<any> {
+    return this.http.get(API_URL + 'admin', {
+      responseType: 'text'
+    });
+  }
+
+
 }
